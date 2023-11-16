8de0358f5a0a2177f5b077a8d315c5245efb9daf
Carl Neuhaus <carlneuhaus@gmail.com>
2023-11-16 17:58:39 +1100

    first

--- app/main.py
+++ app/main.py
@@ -1,15 +1,94 @@
 from flask import Blueprint, render_template, request, Response
+import requests
 import ldap
+import json
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
-def landing():
-    return render_template('index.html')
+endpoint = "https://api.corelogic.asia"
+client_id = "jqhrXN8FIcA8LgMHvijARfAqzy0PD2Cp"
+client_secret = "qWSAAO7n0XPjhMvT"
+token = ""
 
-@main.route('/data', methods = ['POST', 'GET'])
-def process_request():
-    if request.method != 'POST':
-        return Response("Method not supported", status=405)
-    form_data = request.form
-    return render_template('data.html', form_data=form_data)
+@main.route("/")
+def landing():
+    return render_template("index.html")
+
+@main.route("/addressSearch")
+def addressSearch():
+    address = request.args.get('addressSearch')
+    address = address.replace(" Australia","")
+    address = address.replace(',','')
+    address = address.replace(' ','%20')
+    login = checkauth()
+    core_address = get_property_id(address)
+    property_id = core_address['suggestions'][0]['propertyId']
+    return_data = {
+        "Document": get_property_report(property_id)['url'],
+        "Guide": "100"
+    }
+    return_data = {
+            "Document": get_property_report(property_id)['url'],
+            "Guide": get_price_guide(property_id)
+        }
+    return render_template('data.html', form_data=return_data)
+
+def get_price_guide(propertyID):
+    data_uri = f"/property-details/au/properties/{propertyID}/otm/campaign/sales"
+    headers = {'Authorization': f'Bearer {token}'}
+    resp = requests.get(endpoint + data_uri, headers=headers)
+    if resp.status_code == 200:
+        json_data = json.loads(resp.text)
+        return json_data['forSalePropertyCampaign']['campaigns'][0]['firstPublishedPrice']
+
+    return None
+
+def get_property_id(address):
+    data_uri = f"/property/au/v2/suggest.json?q={address}"
+    headers = {'Authorization': f'Bearer {token}'}
+    resp = requests.get(endpoint + data_uri, headers=headers)
+    if resp.status_code == 200:
+        json_data = json.loads(resp.text)
+        return json_data
+
+def get_property_report(propertyID):
+    data = {
+        "customerName": "test",
+        "reportRequester": {
+            "email": "test@test.com",
+            "name": "test",
+            "phone": "phone"
+        }
+    }
+    data = json.dumps(data)
+    data_uri = f"/property-profile-report/au/property/{propertyID}/ppr"
+    headers = {'Authorization': f'Bearer {token}',
+                'Content-Type': 'application/json'
+                }
+    resp = requests.post(endpoint + data_uri, headers=headers, data=data)
+    if resp.status_code == 200:
+        json_data = json.loads(resp.text)
+        return json_data
+
+    return None
+
+def checkauth():
+    global token
+    headers = {'Authorization': f'Bearer {token}'}
+    resp = requests.get(endpoint+"/property-details", headers=headers)
+    print(resp.status_code)
+    if resp.status_code == 401:
+        print("Not logged in, logging in")
+        token = login()
+        return True
+    print("Already logged in, proceeding")
+    return True
+
+def login(clientID=client_id, clientSecret=client_secret):
+    login_uri = f"/access/oauth/token?grant_type=client_credentials&client_id={clientID}&client_secret={clientSecret}"
+    resp = requests.get(endpoint + login_uri)
+    if resp.status_code == 200:
+        json_data = json.loads(resp.text)
+        return json_data['access_token']
+        
+    return None
---
+++ app/static/address.js
@@ -0,0 +1,37 @@
+// This sample uses the Places Autocomplete widget to:
+// 1. Help the user select a place
+// 2. Retrieve the address components associated with that place
+// 3. Populate the form fields with those address components.
+// This sample requires the Places library, Maps JavaScript API.
+// Include the libraries=places parameter when you first load the API.
+// For example: <script
+// src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+let autocomplete;
+let address1Field;
+let address2Field;
+let postalField;
+
+function initAutocomplete() {
+  address1Field = document.querySelector("#addressSearch");
+  address2Field = document.querySelector("#address2");
+  postalField = document.querySelector("#postcode");
+  // Create the autocomplete object, restricting the search predictions to
+  // addresses in the US and Canada.
+  autocomplete = new google.maps.places.Autocomplete(address1Field, {
+    componentRestrictions: { country: ["au"] },
+    fields: ["address_components", "geometry"],
+    types: ["address"],
+  });
+  address1Field.focus();
+  // When the user selects an address from the drop-down, populate the
+  // address fields in the form.
+  autocomplete.addListener("place_changed", sendAddress);
+}
+
+function sendAddress() {
+  // Get the place details from the autocomplete object.
+  const place = autocomplete.getPlace();
+  console.log(place.address_components);
+}
+
+window.initAutocomplete = initAutocomplete;
